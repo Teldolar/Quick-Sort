ef4a07f8b421385a2facd8d6f2326a5a34c696d6
artur <arturgareev2000artur@gmail.com>
2021-03-10 12:42:34 +0300

    bip

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Library1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var quickSortArray = new int[10]
+            {
+                1234,345,63413,72547,2,534,61,275,345,72
+            };
+            PrintArray(quickSortArray);
+            Console.WriteLine("---------------------");
+            quickSortArray = QuickSortClass.quicksort(quickSortArray,0,quickSortArray.Length-1);
+            PrintArray(quickSortArray);
+        }
+
+        private static void PrintArray(int[] myArray)
+        {
+            foreach (var t in myArray)
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
---
+++ Quick Sort/Program.fs
@@ -0,0 +1,7 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+using 
+let list1 = [2; 34; 56; 12; 1; 5; 100; 450; 788 ]
+
+let res = quicksort list1
